07f9d8bd66a017068197d8179220f55e94fcf8c2
MarSeliN666 <44782699+MarSeliN666@users.noreply.github.com>
2019-04-08 21:12:30 +0300

    Add files via upload

---
+++ HomeWork25.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class HomeWork25 {
+    public static void main(String[] args) {
+        int i = 1;
+        int z = 1;
+        while (i <= 5){
+            System.out.println();
+            i++;
+            while (z <= 5){
+                System.out.print(z);
+                z++;
+            }
+        }
+    }
+}
+/*
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            int j;
+            for ( j = 5; j >= i ; j--) {
+                System.out.print(j - i + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+ */
+
